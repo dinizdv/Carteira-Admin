5a33f659ab522803820e4243079f8c7a19cd48e8
dinizdv <dinizdev3@gmail.com>
2024-04-14 10:52:31 -0300

    feat(home): atualizações na sidebar e criação de um menu suspenso.

--- portal/src/pages/Home/index.jsx
+++ portal/src/pages/Home/index.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 // import { Link } from 'react-router-dom';
 import './home.css';
 import '../../App.css'
-// import usuarios from '../../assets/usuarios.png'
+import usuarios from '../../assets/usuarios.png'
 // import horarios from '../../assets/horarios.png'
-// import cursos from '../../assets/cursos.png'
+import cursos from '../../assets/cursos.png'
 // import notificacoes from '../../assets/notificacoes.png'
 // import senaiLogo from '../../assets/senai-logo.png'
 // import BarChart from './home'; 
 
 export default function Home(){
 
-    return(
+return(
   <div>
     
       <div className="wrapper">
@@ -46,17 +46,77 @@ export default function Home(){
                 </ul>
               </li>
 
+              <li className="sidebar-item">
+                <a href="#" className="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse" aria-expanded="false">
+                <i class="fa-regular fa-file-lines pe-2"></i>
+                  Posts
+                </a>
+                <ul id="posts" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                  <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">Post 1</a>
+                  </li>
+                  <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">Post 2</a>
+                  </li>
+                  <li className="sidebar-item">
+                    <a href="#" className="sidebar-link">Post 3</a>
+                  </li>
+                </ul>
+              </li>
+
             </ul>
           </div>
         </aside>
 {/* end sidebar */}
 
+{/* dropdown menu */}
       <div className="main">
-        <nav className="navbar">
-          <button className="btn">
+        <nav className="navbar navbar-expand px-3 border-bottom">
+          <button className="btn" id="sidebar-toggle" type="button">
             <span class="navbar-toggler-icon"></span>
           </button>
+          <div className="navbar-collapse navbar">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item dropdown">
+                <a href="#" className="nav-icon pe-md-0" data-bs-toggle="dropdown">
+                <i className="fa-solid fa-user avatar text-center"></i>
+                </a>
+                <div className="dropdown-menu dropdown-menu-end">
+                  <a href="#" class="dropdown-item">Profile</a>
+                  <a href="#" class="dropdown-item">Settings</a>
+                  <a href="#" class="dropdown-item">Logout</a>
+                </div>
+              </li>
+            </ul>
+          </div>
         </nav>
+        {/* <main className="content px-3 py-2">
+          <div className="container-fluid">
+            <div className="mb-3">
+              Admin Dashboard
+            </div>
+            <div className="row">
+              <div className="col-12 col-md-6 d-flex">
+                <div className="card flex-fill border-0 illustration">
+                  <div className="card-body p-0 d-flex flex-fill">
+                    <div className="row g-0 w-100">
+                      <div className="col-6">
+                        <div className="p-3 m-1">
+                          <h4>Welcome Back, Admin</h4>
+                          <p className="mb-0">Admin Dashboard, SENAI-SP</p>
+                        </div>
+                      </div>
+                      <div className="col-6 align-self-end text-end">
+                        <img src={usuarios} alt="usuários" id="img-users" class="img-fluid illustration-img" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </main> */}
+
       </div>
       </div>
 
