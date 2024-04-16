b0f34392c81a7f164bd80910466cffef6c87c8e1
dinizdv <dinizdev3@gmail.com>
2024-04-16 11:25:48 -0300

    feat(Home): estilização da página home.

--- portal/src/pages/Home/index.jsx
+++ portal/src/pages/Home/index.jsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from 'react';
 import React from 'react';
 // import { Link } from 'react-router-dom';
 import './home.css';
 import '../../App.css';
+
 import usuarios from '../../assets/usuarios.png'
 // import horarios from '../../assets/horarios.png'
 import cursos from '../../assets/cursos.png'
@@ -9,123 +11,281 @@ import cursos from '../../assets/cursos.png'
 // import senaiLogo from '../../assets/senai-logo.png'
 // import BarChart from './home'; 
 
+
 export default function Home(){ 
 
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
+  const [theme, setTheme] = useState(localStorage.getItem('light') ? 'light' : 'dark');
+
+  useEffect(() => {
+      const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+      if (currentTheme !== theme) {
+          document.documentElement.setAttribute('data-bs-theme', theme);
+      }
+  }, [theme]);
+
+  const toggleSidebar = () => {
+      setIsSidebarCollapsed(!isSidebarCollapsed);
+  };
+
+  const toggleTheme = () => {
+      const newTheme = theme === 'light' ? 'dark' : 'light';
+      setTheme(newTheme);
+      localStorage.setItem('light', newTheme === 'light' ? 'set' : null);
+  };
+      
+ 
 return(
+  
   <div>
-    
-      <div className="wrapper">
-        <aside id="sidebar">
-          {/* sidebar content */}
-          <div className="h-100">
-            <div className="sidebar-logo">
-              <a href="#">SENAI-SP</a>
+
+<div class="wrapper">
+        <aside id="sidebar" class="js-sidebar">
+            {/* <!-- Content For Sidebar --> */}
+            <div class="h-100">
+                <div class="sidebar-logo">
+                    <a href="#">CodzSword</a>
+                </div>
+                <ul class="sidebar-nav">
+                    <li class="sidebar-header">
+                        Admin Elements
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link">
+                            <i class="fa-solid fa-list pe-2"></i>
+                            Dashboard
+                        </a>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-target="#pages" data-bs-toggle="collapse"
+                            aria-expanded="false"><i class="fa-solid fa-file-lines pe-2"></i>
+                            Pages
+                        </a>
+                        <ul id="pages" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Page 1</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Page 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse"
+                            aria-expanded="false"><i class="fa-solid fa-sliders pe-2"></i>
+                            Posts
+                        </a>
+                        <ul id="posts" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Post 1</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Post 2</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Post 3</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-target="#auth" data-bs-toggle="collapse"
+                            aria-expanded="false"><i class="fa-regular fa-user pe-2"></i>
+                            Auth
+                        </a>
+                        <ul id="auth" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Login</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Register</a>
+                            </li>
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link">Forgot Password</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="sidebar-header">
+                        Multi Level Menu
+                    </li>
+                    <li class="sidebar-item">
+                        <a href="#" class="sidebar-link collapsed" data-bs-target="#multi" data-bs-toggle="collapse"
+                            aria-expanded="false"><i class="fa-solid fa-share-nodes pe-2"></i>
+                            Multi Dropdown
+                        </a>
+                        <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                            <li class="sidebar-item">
+                                <a href="#" class="sidebar-link collapsed" data-bs-target="#level-1"
+                                    data-bs-toggle="collapse" aria-expanded="false">Level 1</a>
+                                <ul id="level-1" class="sidebar-dropdown list-unstyled collapse">
+                                    <li class="sidebar-item">
+                                        <a href="#" class="sidebar-link">Level 1.1</a>
+                                    </li>
+                                    <li class="sidebar-item">
+                                        <a href="#" class="sidebar-link">Level 1.2</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </div>
-            <ul className="sidebar-nav">
-              <li className="sidebar-header">
-                Admin elements
-              </li>
-              <li className="sidebar-item">
-                <a href="#" className="sidebar-link">
-                <i class="fa-solid fa-list pe-2"></i>
-                  Dashboard
-                </a>
-              </li>
-              <li className="sidebar-item">
-                <a href="#" className="sidebar-link collapsed" data-bs-target="#pages" data-bs-toggle="collapse" aria-expanded="false">
-                <i class="fa-regular fa-file-lines pe-2"></i>
-                  Pages
-                </a>
-                <ul id="pages" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                  <li className="sidebar-item">
-                    <a href="#" className="sidebar-link">Page 1</a>
-                  </li>
-                  <li className="sidebar-item">
-                    <a href="#" className="sidebar-link">Page 2</a>
-                  </li>
-                </ul>
-              </li>
-
-              <li className="sidebar-item">
-                <a href="#" className="sidebar-link collapsed" data-bs-target="#posts" data-bs-toggle="collapse" aria-expanded="false">
-                <i class="fa-regular fa-file-lines pe-2"></i>
-                  Posts
-                </a>
-                <ul id="posts" className="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
-                  <li className="sidebar-item">
-                    <a href="#" className="sidebar-link">Post 1</a>
-                  </li>
-                  <li className="sidebar-item">
-                    <a href="#" className="sidebar-link">Post 2</a>
-                  </li>
-                  <li className="sidebar-item">
-                    <a href="#" className="sidebar-link">Post 3</a>
-                  </li>
-                </ul>
-              </li>
-
-            </ul>
-          </div>
         </aside>
-{/* end sidebar */}
+        <div class="main">
+            <nav class="navbar navbar-expand px-3 border-bottom">
+              {/* button -> responsive sidebar */}
+              <button className="btn" id="sidebar-toggle" type="button" onClick={() => document.getElementById('sidebar').classList.toggle('collapsed')}>
+                  <span className="navbar-toggler-icon"></span>
+              </button>
 
-{/* dropdown menu */}
-      <div className="main">
-        <nav className="navbar navbar-expand px-3 border-bottom">
-          <button className="btn" id="sidebar-toggle" type="button">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div className="navbar-collapse navbar">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item dropdown">
-                <a href="#" className="nav-icon pe-md-0" data-bs-toggle="dropdown">
-                <i className="fa-solid fa-user avatar text-center"></i>
-                </a>
-                <div className="dropdown-menu dropdown-menu-end">
-                  <a href="#" class="dropdown-item">Profile</a>
-                  <a href="#" class="dropdown-item">Settings</a>
-                  <a href="#" class="dropdown-item">Logout</a>
+                <div class="navbar-collapse navbar">
+                    <ul class="navbar-nav">
+                        <li class="nav-item dropdown">
+                            <a href="#" data-bs-toggle="dropdown" class="nav-icon pe-md-0">
+                                {/* <img src="image/profile.jpg" class="avatar img-fluid rounded" alt=""> */}
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-end">
+                                <a href="#" class="dropdown-item">Profile</a>
+                                <a href="#" class="dropdown-item">Setting</a>
+                                <a href="#" class="dropdown-item">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
-              </li>
-            </ul>
-          </div>
-        </nav>
-
-        <a href="#" class="theme-toggle">
-          <i className="fa-regular fa-moon"></i>
-          <i className="fa-regular fa-sun"></i>
-        </a>
-        {/* <main className="content px-3 py-2">
-          <div className="container-fluid">
-            <div className="mb-3">
-              Admin Dashboard
-            </div>
-            <div className="row">
-              <div className="col-12 col-md-6 d-flex">
-                <div className="card flex-fill border-0 illustration">
-                  <div className="card-body p-0 d-flex flex-fill">
-                    <div className="row g-0 w-100">
-                      <div className="col-6">
-                        <div className="p-3 m-1">
-                          <h4>Welcome Back, Admin</h4>
-                          <p className="mb-0">Admin Dashboard, SENAI-SP</p>
-                        </div>
-                      </div>
-                      <div className="col-6 align-self-end text-end">
-                        <img src={usuarios} alt="usuários" id="img-users" class="img-fluid illustration-img" />
-                      </div>
+            </nav>
+            <main class="content px-3 py-2">
+                <div class="container-fluid">
+                    <div class="mb-3">
+                        <h4 class="text-light">Admin Dashboard</h4>
                     </div>
-                  </div>
+                    <div class="row">
+                        <div class="col-12 col-md-6 d-flex">
+                            <div class="card flex-fill border-0 illustration">
+                                <div class="card-body p-0 d-flex flex-fill">
+                                    <div class="row g-0 w-100">
+                                        <div class="col-6">
+                                            <div class="p-3 m-1">
+                                                <h4>Welcome Back, Admin</h4>
+                                                <p class="mb-0">Admin Dashboard, CodzSword</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-6 align-self-end text-end">
+                                            {/* <img src="image/customer-support.jpg" class="img-fluid illustration-img" */}
+                                                {/* alt=""> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-md-6 d-flex">
+                            <div class="card flex-fill border-0">
+                                <div class="card-body py-4">
+                                    <div class="d-flex align-items-start">
+                                        <div class="flex-grow-1">
+                                            <h4 class="mb-2">
+                                                $ 78.00
+                                            </h4>
+                                            <p class="mb-2">
+                                                Total Earnings
+                                            </p>
+                                            <div class="mb-0">
+                                                <span class="badge text-success me-2">
+                                                    +9.0%
+                                                </span>
+                                                <span class="text-muted">
+                                                    Since Last Month
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <!-- Table Element --> */}
+                    <div class="card border-0">
+                        <div class="card-header">
+                            <h5 class="card-title">
+                                Basic Table
+                            </h5>
+                            <h6 class="card-subtitle text-muted">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum ducimus,
+                                necessitatibus reprehenderit itaque!
+                            </h6>
+                        </div>
+                        <div class="card-body">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th scope="col">#</th>
+                                        <th scope="col">First</th>
+                                        <th scope="col">Last</th>
+                                        <th scope="col">Handle</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">1</th>
+                                        <td>Mark</td>
+                                        <td>Otto</td>
+                                        <td>@mdo</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">2</th>
+                                        <td>Jacob</td>
+                                        <td>Thornton</td>
+                                        <td>@fat</td>
+                                    </tr>
+                                    <tr>
+                                        <th scope="row">3</th>
+                                        <td colspan="2">Larry the Bird</td>
+                                        <td>@twitter</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </main> */}
+            </main>
+            {/* dark mode */}
+            <a href="#" class="theme-toggle" onClick={toggleTheme}>
+                {theme === 'light' ? (
+                <i class="fa-regular fa-moon"></i>
+                ) : (
+                <i class="fa-regular fa-sun"></i> )}
+            </a>
+                  
 
-      </div>
-      </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row text-muted">
+                        <div class="col-6 text-start">
+                            <p class="mb-0">
+                                <a href="#" class="text-muted">
+                                    <strong>CodzSwod</strong>
+                                </a>
+                            </p>
+                        </div>
+                        <div class="col-6 text-end">
+                            <ul class="list-inline">
+                                <li class="list-inline-item">
+                                    <a href="#" class="text-muted">Contact</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="text-muted">About Us</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="text-muted">Terms</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="#" class="text-muted">Booking</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
+    </div>
+    </div>
     )
 }
