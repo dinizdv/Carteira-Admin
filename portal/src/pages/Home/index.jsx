d3dec117fa58b65669533052741e962fcd7eec29
dinizdv <dinizdev3@gmail.com>
2024-04-16 15:11:35 -0300

    feat(Home): atualizações na interface.

--- portal/src/pages/Home/index.jsx
+++ portal/src/pages/Home/index.jsx
@@ -158,14 +158,14 @@ return(
                         <h4 class="text-light">Admin Dashboard</h4>
                     </div>
                     <div class="row">
-                        <div class="col-12 col-md-6 d-flex">
-                            <div class="card flex-fill border-0 illustration">
+                        <div class="col-12 col-md-6 col-xl-3 d-flex">
+                            <div class="card flex-fill border-0 illustration card-info">
                                 <div class="card-body p-0 d-flex flex-fill">
                                     <div class="row g-0 w-100">
-                                        <div class="col-6">
+                                        <div class="col-9">
                                             <div class="p-3 m-1">
-                                                <h4>Welcome Back, Admin</h4>
-                                                <p class="mb-0">Admin Dashboard, CodzSword</p>
+                                                <h3>Usuários</h3>
+                                                <p class="mb-0">Editar, adicionar, excluir</p>
                                             </div>
                                         </div>
                                         <div class="col-6 align-self-end text-end">
@@ -176,30 +176,65 @@ return(
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-6 d-flex">
-                            <div class="card flex-fill border-0">
-                                <div class="card-body py-4">
-                                    <div class="d-flex align-items-start">
-                                        <div class="flex-grow-1">
-                                            <h4 class="mb-2">
-                                                $ 78.00
-                                            </h4>
-                                            <p class="mb-2">
-                                                Total Earnings
-                                            </p>
-                                            <div class="mb-0">
-                                                <span class="badge text-success me-2">
-                                                    +9.0%
-                                                </span>
-                                                <span class="text-muted">
-                                                    Since Last Month
-                                                </span>
+
+                        <div class="col-12 col-md-6 col-xl-3 d-flex">
+                            <div class="card flex-fill border-0 illustration card-info">
+                                <div class="card-body p-0 d-flex flex-fill">
+                                    <div class="row g-0 w-100">
+                                        <div class="col-9">
+                                            <div class="p-3 m-1">
+                                                <h3>Cursos</h3>
+                                                <p class="mb-0">Nome, horário</p>
                                             </div>
+                                        </div>
+                                        <div class="col-6 align-self-end text-end">
+                                            {/* <img src="image/customer-support.jpg" class="img-fluid illustration-img" */}
+                                                {/* alt=""> */}
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
+
+                        <div class="col-12 col-md-6 col-xl-3 d-flex">
+                            <div class="card flex-fill border-0 illustration card-info">
+                                <div class="card-body p-0 d-flex flex-fill">
+                                    <div class="row g-0 w-100">
+                                        <div class="col-9">
+                                            <div class="p-3 m-1">
+                                                <h3>Undefined</h3>
+                                                <p class="mb-0">Nome, horário</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-6 align-self-end text-end">
+                                            {/* <img src="image/customer-support.jpg" class="img-fluid illustration-img" */}
+                                                {/* alt=""> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-xl-3 d-flex">
+                            <div class="card flex-fill border-0 illustration card-info">
+                                <div class="card-body p-0 d-flex flex-fill">
+                                    <div class="row g-0 w-100">
+                                        <div class="col-9">
+                                            <div class="p-3 m-1">
+                                                <h3>Notificações</h3>
+                                                <p class="mb-0">Notificações gerais, notificações individuais</p>
+                                            </div>
+                                        </div>
+                                        <div class="col-6 align-self-end text-end">
+                                            {/* <img src="image/customer-support.jpg" class="img-fluid illustration-img" */}
+                                                {/* alt=""> */}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
                     {/* <!-- Table Element --> */}
                     <div class="card border-0">
