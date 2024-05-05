263e1d7dbc7b2fc83cc8b9c5d225e0e75c4355a1
dinizdv <dinizdev3@gmail.com>
2024-05-05 15:38:31 -0300

    feat(Testing): update code.

--- portal/src/pages/Testing/testing.jsx
+++ portal/src/pages/Testing/testing.jsx
@@ -1,50 +1,36 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+const fetchData = async () => {
+  try {
+     const apiUrl = 'https://apicontroleacesso-1.onrender.com/usuario';
+     const response = await axios.get(apiUrl);
+     const userData = {
+       cpf: response.data.cpf,
+       email: response.data.email,
+       senha: response.data.senha,
+     };
+     setUsers([userData]); // Atualiza o estado com os dados do usuário
+  } catch (error) {
+     console.error('Error fetching data:', error);
+  }
+ };
 
-const Testing = () => {
-  const [users, setUsers] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    try {
-      const apiUrl = 'https://api-controle-acesso-latest.onrender.com/usuario/1';
-      const response = await axios.get(apiUrl);
-      const userData = response.data.content.map(user => ({
-        id: user.id,
-        nome: user.nome,
-        cpf: user.cpf,
-        email: user.email
-      }));
-      setUsers(userData);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  return (
-    <div>
-      {users.length > 0 ? (
-        <div>
-          <h3>Informações dos Usuários:</h3>
-          <ul>
-            {users.map(user => (
-              <li key={user.id}>
-                <p>ID: {user.id}</p>
-                <p>Nome: {user.nome}</p>
-                <p>CPF: {user.cpf}</p>
-                <p>Email: {user.email}</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ) : (
-        <p>Carregando...</p>
-      )}
-    </div>
-  );
-};
-
-export default Testing;
+ return (
+  <div>
+     {users.length > 0 ? (
+       <div>
+         <h3>Informações do Usuário:</h3>
+         <ul>
+           {users.map(user => (
+             <li key={user.cpf}>
+               <p>CPF: {user.cpf}</p>
+               <p>Email: {user.email}</p>
+               <p>Senha: {user.senha}</p>
+             </li>
+           ))}
+         </ul>
+       </div>
+     ) : (
+       <p>Carregando...</p>
+     )}
+  </div>
+ );
+ 
